44c2e5af0c8d22e60b69df9b3b705fce015025d4
Diego Bogado <diegobogado@live.com.ar>
2013-03-30 20:40:00 -0300

    1er commit
    String Calculator Kata resuelto con C# y NUnit.

---
+++ StringCalculatorKataDiaTres.Library/Calculator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculatorKataDiaTres.Library
+{
+    public class Calculator
+    {
+        public int Add(string value)
+        {
+            int add;
+
+            if (CheckIfIsEmptyString(value, out add)) return add;
+
+            if (IsNegative(value))            
+                throw new ArgumentException("negatives not allowed");
+            
+            var separators = GetSeparators(ref value);
+                      
+            var numbers = value.Split(separators,StringSplitOptions.None).Where(x => Convert.ToInt32(x) < 1001);
+           return numbers.Sum(x => Convert.ToInt32(x));
+        }
+
+        private bool IsNegative(string value)
+        {
+            return value.IndexOf('-') == 0 && IsANumber(value.Substring(value.IndexOf('-')));
+        }
+
+        private bool IsANumber(string value)
+        {
+            int result;
+            return Int32.TryParse(value,out result);
+        }
+
+        private static string[] GetSeparators(ref string value)
+        {
+            string[] separators;
+            if (value.Contains("//"))
+            {
+                
+                if (value.IndexOf('[') == 2)
+                {
+                    int i = 0;
+                    separators = new string[GetSeparatorsNumber(value.Substring(2,value.IndexOf('\n') - 2))];
+                    var sep = value.Substring(2, value.IndexOf('\n') - 2).Split('[', ']').Where(x => x != string.Empty);
+                    foreach (var separator in sep)
+                    {
+                        separators[i] = separator;
+                        i++;
+                    }                   
+                }                    
+                else
+                {
+                    separators=new string[1];
+                    separators[0] = value.Substring(2, value.IndexOf('\n') - 2);   
+                }
+                                                         
+                value = value.Substring(value.IndexOf('\n'));
+            }
+            else
+            {
+                separators = new string[2];
+                separators[0] = ",";
+                separators[1] = "\n";
+            }
+            return separators;
+        }
+
+        private static int GetSeparatorsNumber(string value)
+        {
+            return value.Split('[',']').Count(x => x != string.Empty);
+        }
+
+        private static bool CheckIfIsEmptyString(string value, out int add)
+        {
+            add = 0;
+            return value == string.Empty;
+        }
+    }
+}
---
+++ StringCalculatorKataDiaTres.Test/CalculatorTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using StringCalculatorKataDiaTres.Library;
+
+namespace StringCalculatorKataDiaTres.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Test]
+        public void ShouldReturnZeroWhenIsEmptyString()
+        {
+            Assert.AreEqual(0,_calculator.Add(string.Empty));
+        }
+
+        [Test]
+        public void ShouldReturnTheSameNumber()
+        {
+            Assert.AreEqual(1, _calculator.Add("1"));
+            Assert.AreEqual(2, _calculator.Add("2"));
+        }
+
+        [Test]
+        public void ShouldReturnTheSum()
+        {
+            Assert.AreEqual(3, _calculator.Add("1,2"));
+            Assert.AreEqual(5, _calculator.Add("2,3"));
+        }
+
+        [Test]
+        public void ShouldAcceptANewSeparator()
+        {
+            Assert.AreEqual(6, _calculator.Add("1\n2,3"));            
+        }
+
+        [Test]
+        public void ShouldAddANewSeparator()
+        {
+            Assert.AreEqual(3, _calculator.Add("//;\n1;2"));
+        }
+
+        [Test]
+        public void ShouldThrowAnExceptionWhenIsNegative()
+        {
+            try
+            {
+                _calculator.Add("-1");
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual("negatives not allowed",e.Message);                
+            }
+        }
+
+        [Test]
+        public void ShouldNotAddNumbersBiggerThan1000()
+        {
+            Assert.AreEqual(2, _calculator.Add("1001,2"));
+        }
+
+        [Test]
+        public void ShouldAddANewSeparatorOfAnyLength()
+        {
+            Assert.AreEqual(6, _calculator.Add("//[***]\n1***2***3"));
+        }
+
+        [Test]
+        public void ShouldVariousSeparators()
+        {
+            Assert.AreEqual(6, _calculator.Add("//[*][%]\n1*2%3"));
+        }
+
+        [Test]
+        public void ShouldVariousSeparatorsOfAnyLength()
+        {
+            Assert.AreEqual(6, _calculator.Add("//[******][%%%]\n1******2%%%3"));
+        }
+    }    
+}
